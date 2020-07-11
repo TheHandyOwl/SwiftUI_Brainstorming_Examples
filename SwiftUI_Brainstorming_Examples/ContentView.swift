a043c1b914ffd970d457ec734b85bc22d233e63d
The Handy Owl <thehandyowl.io@gmail.com>
2020-07-11 12:20:39 +0200

    Initial Commit

---
+++ SwiftUI_Brainstorming_Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Brainstorming_Examples
+//
+//  Created by Carlos on 11/07/2020.
+//  Copyright © 2020 TestCompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
